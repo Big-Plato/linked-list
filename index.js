976df7ac5cb165b4c512e8396b78ebd63d077fec
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-27 13:33:22 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = null;
+        this.nextNode = null;
+    }
+
+    append = () => {
+
+    }
+
+    prepend = () => {
+
+    }
+
+    size = () => {
+
+    }
+
+    head = () => {
+
+    }
+
+    tail = () => {
+
+    }
+
+    at = (index) => {
+
+    }
+
+    pop = () => {
+
+    }
+
+    contains = (value) => {
+
+    } 
+
+    find = (value) => {
+
+    }
+}
