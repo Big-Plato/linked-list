7264e70c80b3a938f78b945867415e1391b9ca4b
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-28 18:48:54 -0300

    class linkedlist finished

--- index.js
+++ index.js
@@ -2,47 +2,175 @@ class LinkedList {
     constructor(head = null) {
         this.head = head;
     }
-}
 
-class Node {
-    constructor(value) {
-        this.value = null;
-        this.nextNode = null;
+    append = (value) => {
+        const node = new Node(value);
+        const tail = this.tail();
+        if (this.head === null) { 
+            this.head = node;
+        } else if (tail.next === null) { 
+            tail.next = node;
+        }
+        return node;
     }
 
-    append = () => {
-
-    }
-
-    prepend = () => {
-
+    prepend = (value) => {
+        const node = new Node(value);
+        node.next = this.head;
+        this.head = node;
     }
 
     size = () => {
+        let count = 0;
+        let temp = this.head;
 
+        while (temp !== null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
     }
 
     head = () => {
-
+        return this.head;
     }
 
     tail = () => {
-
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next;
+            }
+        }
+        return lastNode;
     }
 
     at = (index) => {
+        let temp = this.head;
+        let count = 0;
 
+        while (count < index) {  
+            temp = temp.next;
+            if (temp === null) {
+                throw new Error("Index: Out of Bounds");
+            }
+            count++;
+        }
+        return temp;
     }
 
     pop = () => {
+        const tail = this.tail();
 
+        if (!this.head) {
+            throw new Error("Can't delete");
+        }
+
+        if (this.head.next === null) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let cur = this.head; 
+        let prev = null; 
+
+        while (cur !== null) {
+            prev = cur;
+            cur = cur.next;
+
+            if (cur === tail) {
+                prev.next = cur.next;
+            }
+        }
+        
     }
 
-    contains = (value) => {
-
+    contains = (value) => {                
+        let cur = this.head;
+        
+        while (cur !== null) {
+            if (cur.value === value) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
     } 
 
-    find = (value) => {
+    find = (value) => { 
+        let count = 0;
+        let cur = this.head;
+        const size = this.size();
+
+        while (count <= size) {
+            if (cur.value === value) {
+                return count;
+            }
+            count++;
+            cur = cur.next;
+        }
+        return null;      
+    }
+
+    toString = () => { 
+        let cur = this.head;
+        let result = "";
+        while (cur) {
+            result += `( ${cur.value} ) -> `;
+            cur = cur.next;
+        }
+        result += 'null';
+        return result;
+    }
+
+    insertAt = (value, index) => {
+        let cur = this.head;
+        let prev = null;
+        const node = new Node(value);
+        let count = 0;
+        const size = this.size();
+
+        if (index < 0 || index > size + 1) {
+            throw new Error("Index: Out of Bounds");
+        }
+
+        if (index === 0) {
+            this.prepend(value);
+        }
+
+        while (count < index) {
+            prev = cur;
+            cur = cur.next;
+            count++;
+        }
+       
+        prev.next = node;
+        node.next = cur;
 
     }
+
+    removeAt = (index) => {
+        let cur = this.head;
+        let prev = null;
+        let count = 0; 
+
+        while (count < index) {
+            prev = cur;
+            cur = cur.next;
+            count++;
+        }
+
+        if (count === index) {
+            prev.next = cur.next;
+            console.log(count)
+            return;
+        }        
+    }
 }
+
+class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.next = null;
+    }
+} 
